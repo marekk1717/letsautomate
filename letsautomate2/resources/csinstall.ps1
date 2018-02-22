63a7014f96f67a615c8cd1d0bbe1d375d765a52e
marekk <kedzierski.m@gmail.com>
2018-02-22 21:30:04 +0100

    Update csinstall.ps1

--- letsautomate2/resources/csinstall.ps1
+++ letsautomate2/resources/csinstall.ps1
@@ -7,8 +7,11 @@ param (
     [string]$app_id = "XXXX",
     [string]$app_pwd = "XXXX",
     [string]$subscription_id = "XXXX",
-    [string]$tenant_id = "XXXX"
+    [string]$tenant_id = "XXXX",
+    [string]$region = "XXXX"
 )
+
+$regions = ($region -replace '\s','').ToLower()
 
 Import-Module BitsTransfer
 If(!(test-path C:\CVINSTALL)) { New-Item -ItemType directory -Path C:\CVINSTALL }
@@ -19,10 +22,14 @@ Start-Process -Wait -FilePath C:\CVINSTALL\CommvaultSetup.exe -ArgumentList '/s 
 Start-BitsTransfer -Source "http://documentation.commvault.com/commvault/v11/others/features/storage_policies/xml/SP_creation.xml" -Destination "C:\CVINSTALL\SP_creation.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/install.xml" -Destination "C:\Program Files\Commvault\installer\install.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/cloudlib.tmp" -Destination "C:\CVINSTALL\cloudlib.tmp"
-Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/installcs.ps1" -Destination "C:\CVINSTALL\installcs.ps1"
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/installcs.tmp" -Destination "C:\CVINSTALL\installcs.tmp"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/azurerm.tmp" -Destination "C:\CVINSTALL\azurerm.tmp"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/schedule.xml" -Destination "C:\CVINSTALL\schedule.xml"
 Start-BitsTransfer -Source "http://documentation.commvault.com/commvault/v11/others/products/vs_vmware/command_line_xml/create_subclient_template.xml" -Destination "C:\CVINSTALL\create_subclient_template.xml"
 Start-BitsTransfer -Source "http://documentation.commvault.com/commvault/v11/others/products/vs_vmware/command_line_xml/update_vm_filters_template.xml" -Destination "C:\CVINSTALL\update_vm_filters_template.xml"
 (Get-Content "C:\CVINSTALL\cloudlib.tmp")  -replace "storage_account",$saccount -replace "storage_primary_key", $saccountkey -replace "vmname",$env:computername | Set-Content "C:\CVINSTALL\cloudlib.xml"
+(Get-Content "C:\CVINSTALL\installcs.tmp")  -replace "varregion",$region -replace "varsregion", $regions | Set-Content "C:\CVINSTALL\installcs.ps1"
 If ($app_id -ne 'XXXX' -And $app_pwd -ne 'XXXX' -And $subscription_id -ne 'XXXX' -And $tenant_id -ne 'XXXX') { (Get-Content "C:\CVINSTALL\azurerm.tmp")  -replace "vmname",$env:computername -replace "subscription_id",$subscription_id -replace "tenant_id",$tenant_id -replace "app_pwd",$app_pwd | Set-Content "C:\CVINSTALL\azurerm.xml" }
+Remove-Item -Path C:\CVINSTALL\installcs.tmp -Force
+Remove-Item -Path C:\CVINSTALL\cloudlib.tmp -Force
+Remove-Item -Path C:\CVINSTALL\azurerm.tmp -Force
