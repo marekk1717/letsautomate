4ff11562c376d8830cdb686420b6134b53fad5a5
marekk <kedzierski.m@gmail.com>
2018-02-22 17:03:45 +0100

    Update csinstall.ps1

--- letsautomate2/resources/csinstall.ps1
+++ letsautomate2/resources/csinstall.ps1
@@ -1,7 +1,11 @@
 param (
     [string]$saccount = "storageaccount", 
-    [string]$saccountkey = "XXXXXXXX",
+    [string]$saccountkey = "XXXX",
     [string]$adminuser = "adminuser"
+    [string]$app_id = "XXXX"
+    [string]$app_pwd = "XXXX"
+    [string]$subscription_id = "XXXX"
+    [string]$tenant_id = "XXXX"
 )
 
 Import-Module BitsTransfer
@@ -13,5 +17,6 @@ Start-Process -Wait -FilePath C:\CVINSTALL\CommvaultSetup.exe -ArgumentList '/s 
 Start-BitsTransfer -Source "http://documentation.commvault.com/commvault/v11/others/features/storage_policies/xml/SP_creation.xml" -Destination "C:\CVINSTALL\SP_creation.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/install.xml" -Destination "C:\Program Files\Commvault\installer\install.xml"
 Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/cloudlib.tmp" -Destination "C:\CVINSTALL\cloudlib.tmp"
-Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/installcs.ps1 -Destination "C:\Users\$adminuser\installcs.ps1"
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/installcs.ps1" -Destination "C:\Users\$adminuser\installcs.ps1"
+Start-BitsTransfer -Source "https://raw.githubusercontent.com/marekk1717/letsautomate/master/letsautomate2/resources/azurerm.tmp" -Destination "C:\CVINSTALL\azurerm.tmp"
 (Get-Content "C:\CVINSTALL\cloudlib.tmp")  -replace "storage_account",$saccount  -replace "storage_primary_key", $saccountkey -replace "vmname",$env:computername | Set-Content "C:\CVINSTALL\cloudlib.xml"
